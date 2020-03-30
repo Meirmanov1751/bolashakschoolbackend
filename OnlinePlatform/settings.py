344e27f753c78ba5228e514f205e0cfcc5c823ee
galix <galix.kz@gmail.com>
2020-03-30 18:19:17 +0600

    django cors headers

--- OnlinePlatform/settings.py
+++ OnlinePlatform/settings.py
@@ -45,11 +45,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'psycopg2',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
 }
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
